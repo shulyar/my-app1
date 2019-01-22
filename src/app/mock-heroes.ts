79ee60ed23530daef6d7459b2b0fb05e9353fe9b
Admin <shulyar@bigmir.net>
2019-01-22 09:38:44 +0200

    shulyar@bigmir.net

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice', age: 34 },
+  {id: 12, name: 'Narco', age: 31 },
+  {id: 13, name: 'Bombasto', age: 25 },
+  {id: 14, name: 'Celeritas', age: 37 },
+  {id: 15, name: 'Magneta', age: 40 },
+  {id: 16, name: 'RubberMan', age: 13 },
+  {id: 17, name: 'Dynama', age: 20 },
+  {id: 18, name: 'Dr IQ', age: 22 },
+  {id: 19, name: 'Magma', age: 31 },
+  {id: 20, name: 'Tornado', age: 24 }
+
+
+
+  ];
